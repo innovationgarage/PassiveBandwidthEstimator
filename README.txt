d20ec48057933e5d545e527d1f2d0134f38de376
Egil <egil.moller@freecode.no>
2019-04-01 13:15:49 +0200

    Got tcp stream parsing to work

--- README.txt
+++ README.txt
@@ -4,3 +4,9 @@ docker exec -it bandwidthmeasure_h1_1 /bin/bash
 
 docker exec -it bandwidthmeasure_h2_1 /bin/bash
   iperf -s
+
+tcpdump -i eth0 -w dumpfile -C 10
+
+sudo apt install libpcap0.8-dev libpcap0.8
+# pip install pypcap
+pip install pypcapfile
---
+++ pcaptotcpgaps.py
@@ -0,0 +1,47 @@
+import pcapfile.savefile
+import numpy
+
+with open("data", "rb") as f:
+    d = pcapfile.savefile.load_savefile(f, layers=3, verbose=True)
+
+    packets = numpy.zeros(len(d.packets), dtype=[("timestamp", int),
+                                                 ("stream", int),
+                                                 ("direction", bool),
+                                                 ("seqnum", int),
+
+                                                 ("ack", bool),
+                                                 ("acknum", int),
+
+                                                 ("src", "<S15"),
+                                                 ("dst", "<S15"),
+                                                 ("src_port", int),
+                                                 ("dst_port", int)])
+
+    streams = {}
+    next_streamid = 0
+
+    for idx, packet in enumerate((packet for packet in d.packets if packet.packet.payload.p == 6)):            
+        src = (packet.packet.payload.src,
+               packet.packet.payload.payload.src_port)
+        dst = (packet.packet.payload.dst,
+               packet.packet.payload.payload.dst_port)
+
+        streamname = tuple(sorted((src, dst)))
+        if streamname not in streams:
+            streams[streamname] = next_streamid
+            next_streamid += 1
+        streamid = streams[streamname]
+        direction = src == streamname[0]
+        
+        packets[idx]["timestamp"] = packet.timestamp
+        packets[idx]["stream"] = streamid
+        packets[idx]["direction"] = direction
+        packets[idx]["seqnum"] = packet.packet.payload.payload.seqnum
+        packets[idx]["ack"] = packet.packet.payload.payload.ack
+        packets[idx]["acknum"] = packet.packet.payload.payload.acknum
+        packets[idx]["src"] = packet.packet.payload.src
+        packets[idx]["src_port"] = packet.packet.payload.payload.src_port
+        packets[idx]["dst"] = packet.packet.payload.dst
+        packets[idx]["dst_port"] = packet.packet.payload.payload.dst_port
+        
+    numpy.savez_compressed("streams.npz", packets=packets[:idx+1])
